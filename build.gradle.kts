626da5e991119aa3169111e9a14fafd91fdec1c0
Thomas <thomasjamesfowler97@gmail.com>
2021-09-01 19:33:08 +1000

    Bumped version to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "com.tfowl.socketio"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 description = "Kotlin coroutine extensions for the socket.io-client library"
 
 repositories {
